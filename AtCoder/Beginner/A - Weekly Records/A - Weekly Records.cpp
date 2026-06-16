787ede8a4d68a7230725b65e2b400992c23571bc
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:41:17 +0600

    [A - Weekly Records | ABC307] Accepted | Time: 1ms | Memory: 3668KB

---
+++ AtCoder/Beginner/A - Weekly Records/A - Weekly Records.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i;
+    int sum=0;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        int ar[7];
+        for(int j=0;j<7;j++)
+        {
+            cin>>ar[j];
+        }
+        sum=0;
+        
+        for(int j=0;j<7;j++)
+        {
+            sum+=ar[j];
+        }
+        
+        
+        cout<<sum<<' ';
+        
+        
+    }
+}
