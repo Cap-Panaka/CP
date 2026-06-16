4544204355c574f836b1830d5b5027cf787d8d51
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:23:07 +0600

    [2065A - Skibidus and Amog'u | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ Codeforces/Div. 4/2065A - Skibidus and Amog'u/2065A - Skibidus and Amog'u.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+void solve()
+{
+ string s;
+    cin>>s;
+    for(int i=0;i<s.length()-2;i++)
+    {
+        cout<<s[i];
+    }
+    cout<<"i"<<endl;
+    
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
