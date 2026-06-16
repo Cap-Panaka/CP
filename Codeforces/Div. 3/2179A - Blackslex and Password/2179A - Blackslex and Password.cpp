10f01e8a1de6588a02803a61b675ce09d8cb4513
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:30:52 +0600

    [2179A - Blackslex and Password | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ Codeforces/Div. 3/2179A - Blackslex and Password/2179A - Blackslex and Password.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n , m;
+        
+        cin>>n>>m;
+        
+        int res=n*m+1;
+        
+        cout<<res<<endl;
+    }
+}
