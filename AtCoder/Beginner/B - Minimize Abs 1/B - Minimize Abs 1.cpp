01e41a34f6dce5cf1f9258545e568c2287d0f2b7
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:41:00 +0600

    [B - Minimize Abs 1 | ABC330] Accepted | Time: 60ms | Memory: 4280KB

---
+++ AtCoder/Beginner/B - Minimize Abs 1/B - Minimize Abs 1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,l,r;
+    cin>>n>>l>>r;
+    int ar[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>ar[i];
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        if(ar[i]<l) cout<<l<<' ';
+        else if(ar[i]<=r && ar[i] >=l) cout<<ar[i]<<' ';
+        else cout<<r<<' ';
+        
+    }
+    // cout<<'\n';
+}
