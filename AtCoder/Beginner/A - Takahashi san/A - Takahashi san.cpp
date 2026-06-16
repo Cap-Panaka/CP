06b7960862e7ead1d21394d3805848cba3ce3691
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:45:24 +0600

    [A - Takahashi san | ABC325] Accepted | Time: 0ms | Memory: 1704KB

---
+++ AtCoder/Beginner/A - Takahashi san/A - Takahashi san.cpp
@@ -0,0 +1,33 @@
+//https://atcoder.jp/contests/abc325/tasks/abc325_a
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+void solve()
+{ 
+    string s;
+    cin>>s;
+
+    cout<<s<<" "<<"san"<<'\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+   // int t = 1;
+    //cin >> t;
+
+    //while(t--)
+    //{
+        solve();
+    //}
+
+    return 0;
+}
