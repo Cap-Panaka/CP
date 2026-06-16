02a743add30805e6e2c91b2be8f670e03e65f86d
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:34:13 +0600

    [1878A - How Much Does Daytona Cost? | 800] Accepted | Time: 124ms | Memory: 0KB

---
+++ Codeforces/Div. 3/1878A - How Much Does Daytona Cost/1878A - How Much Does Daytona Cost.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int i,n,k;
+        cin>>n>>k;
+        int ar[n];
+        for(i=0;i<n;i++)
+        {
+            cin>>ar[i];
+        }
+        int flag=1;
+        for(i=0;i<n;i++)
+        {
+            if(ar[i]==k)
+            {
+                flag=0;
+            }
+        }
+        
+        if(flag==0)
+        {
+            cout<<"YES"<<'\n';
+        }
+        else
+        {
+            cout<<"NO"<<'\n';
+        }
+    }
+}
