d7e661266debd6df75462e3d843a5c073ede7897
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:24:20 +0600

    [2214D - Neural Feud | Gym] Accepted | Time: 31ms | Memory: 0KB

---
+++ Codeforces/Others/2214D - Neural Feud/2214D - Neural Feud.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+
+	vector<string>ar(9);
+	ar[0]="PANAKA!";
+	ar[1]="Walk";
+	ar[2]="No";
+	ar[3]="No";
+	ar[4]="No";
+	ar[5]="Yes";
+	ar[6]="Yes";
+	ar[7]="backwards";
+	ar[8]="seven";
+	
+	cout<<ar[n]<<endl;
+	
+
+}
