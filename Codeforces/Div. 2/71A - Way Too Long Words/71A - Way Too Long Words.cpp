9ec7a57f046ea89304f5b9d1c6eda08b00da4b7b
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:36:37 +0600

    [71A - Way Too Long Words | 800] Accepted | Time: 61ms | Memory: 0KB

---
+++ Codeforces/Div. 2/71A - Way Too Long Words/71A - Way Too Long Words.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    string s;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>s;
+        if(s.length()>10)
+        {
+            cout<<s[0]<<s.length()-2 <<s[s.length()-1]<<'\n';
+        }
+        else
+        {
+
+            cout <<s<<endl;
+        }
+    }
+
+}
