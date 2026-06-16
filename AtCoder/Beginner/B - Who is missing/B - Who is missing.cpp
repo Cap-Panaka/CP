7e324e976a7a852592c6ebd0a8354939d454ab56
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:40:33 +0600

    [B - Who is missing | ABC236] Accepted | Time: 30ms | Memory: 2116KB

---
+++ AtCoder/Beginner/B - Who is missing/B - Who is missing.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    int count[n+1];
+    for(int i=1;i<=n;i++)
+    {
+        count[i]=0;
+    }
+    
+    for(int i =0;i<n*4-1;i++)
+    {
+        int x;
+        scanf("%d",&x);
+        count[x]++;
+    }
+    
+    int ans ;
+    {
+        for(int i=1;i<=n;i++)
+        {
+            if(count[i]==3) ans=i;
+            
+        }
+        
+    }
+    printf("%d\n",ans);
+}
