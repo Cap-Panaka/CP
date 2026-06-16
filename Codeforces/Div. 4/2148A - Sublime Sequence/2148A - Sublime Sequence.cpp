3a2ddf30496d14e767bfdd5032c8141e01a64b96
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:24:08 +0600

    [2148A - Sublime Sequence | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ Codeforces/Div. 4/2148A - Sublime Sequence/2148A - Sublime Sequence.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n=0,x=0;
+	    cin>>x>>n;
+	    
+	    if(n%2!=0) cout<<x<<endl;
+	    else cout<<0<<endl;
+	}
+
+}
