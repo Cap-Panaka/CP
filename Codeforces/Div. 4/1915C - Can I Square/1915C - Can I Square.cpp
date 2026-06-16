cdcdad825c8561d4ca3a96fc4d41925268471cf3
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:34:21 +0600

    [1915C - Can I Square? | 800] Accepted | Time: 390ms | Memory: 0KB

---
+++ Codeforces/Div. 4/1915C - Can I Square/1915C - Can I Square.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        long long n;
+        cin>>n;
+        int ar[n];
+        long long sum=0;
+        for(int i=0;i<n;i++)
+        {
+            cin>>ar[i];
+            sum=ar[i]+sum;
+        }
+        if(ceil((double)sqrt(sum))==floor((double)sqrt(sum)))
+        {
+            cout<<"YES"<<'\n';
+        }
+        else
+        {
+            cout<<"NO"<<'\n';
+        }
+        
+        
+    }
+}
