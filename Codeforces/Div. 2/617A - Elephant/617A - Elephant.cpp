ac41b694b15a68457b67eadadea943c76f2b0fbe
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:28:39 +0600

    [617A - Elephant | 800] Accepted | Time: 46ms | Memory: 100KB

---
+++ Codeforces/Div. 2/617A - Elephant/617A - Elephant.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int count =0;
+	while(n>0)
+	{
+	    if(n>=5)
+	    {
+	        n-=5;
+	    }
+	    else if(n>=4)
+	    {
+	        n-=4;
+	    }
+	    else if(n>=3)
+	    {
+	        n-=3;
+	    }
+	    else if(n>=2)
+	    {
+	        n-=2;
+	    }
+	    else 
+	    {
+	        n-=1;
+	    }
+	    count ++;
+	}
+	cout<<count<<endl;
+	
+
+}
