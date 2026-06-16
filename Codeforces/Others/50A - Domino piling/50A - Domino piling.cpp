1eb9c51e259b4c1d0bf7408bf26724326e6321fd
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-17 00:29:56 +0600

    [50A - Domino piling | 800] Accepted | Time: 92ms | Memory: 100KB

---
+++ Codeforces/Others/50A - Domino piling/50A - Domino piling.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    
+    cout<<n*m/2<<endl;
+    /*
+    
+    if(n*m % 2 == 0)
+    {
+        cout<< n*m /2 <<endl;
+    }
+    
+    else
+    {
+        cout<< (n*m ) /2 <<endl;
+    }
+    
+    */
+	
+
+}
