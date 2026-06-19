df5026dd8b8a1f9d85be76d5cce4430a3b32b1b1
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-06-19 22:26:22 +0600

    [144A - Arrival of the General | 800] Accepted | Time: 92ms | Memory: 0KB

--- Codeforces/Div. 2/144A - Arrival of the General/144A - Arrival of the General.cpp
+++ Codeforces/Div. 2/144A - Arrival of the General/144A - Arrival of the General.cpp
@@ -1,45 +1,58 @@
-#include<bits/stdc++.h>
+
+#include <bits/stdc++.h>
 using namespace std;
-int ar[1001];
-int n;
-int max_idx()
-{
-    int max=0;
-    for(int i=1;i<n;i++)
-    {
-        if(ar[i]>ar[max])
-        {
-            max=i;
-        }
-    }
-    return max;
-}
-int min_idx()
-{
-    int min=0;
-    for(int i=1;i<n;i++)
-    {
-        if(ar[i]<=ar[min])
-        {
-            min=i;
-        }
-    }
-    return min;
-}
 
+#define ll long long
+#define endl '\n'
 
+void solve()
+{ 
+    int n;
+    cin>>n;
+    vector<int>ar(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>ar[i];
+    }
+
+    int maxPos=0;
+    int minPos=0;
+
+    for(int i=0;i<n;i++)
+    {
+        if(ar[i]>ar[maxPos])
+        {
+            maxPos=i;
+        }
+
+        if(ar[i]<=ar[minPos])
+        {
+            minPos=i;
+        }
+
+    }
+    int ans = maxPos + (n-1-minPos);
+    if(maxPos>minPos)
+    {
+        ans--;
+    }
+    cout<<ans<<endl;
+
+}
 
 int main()
 {
-    scanf("%d",&n);
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&ar[i]);
-    }
-    
-    int max=max_idx();
-    int min=min_idx();
-    int res=max+(n-min-1);
-    if(max>min) res--;
-    printf("%d\n",res);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+   // int t = 1;
+    //cin >> t;
+
+    //while(t--)
+    //{
+        solve();
+    //}
+
+    return 0;
 }
