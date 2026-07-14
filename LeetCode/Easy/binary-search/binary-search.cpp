3f7043d09d4155b638d34ead67c30f465c1d5cdd
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-07-14 10:29:19 +0500

    Time: 0ms | Memory: 0MB | Language: cpp

--- LeetCode/Easy/binary-search/binary-search.cpp
+++ LeetCode/Easy/binary-search/binary-search.cpp
@@ -1,26 +1,24 @@
-class Solution {
-public:
-    int search(vector<int>& a, int target) {
-        int l=0;
-        int r=a.size()-1;
+    int search(vector<int>& a, int target) {
+        int l=0;
+        int r=a.size()-1;
 
-        while(l<=r)
-        {
-            int mid = l+(r-l)/2;
-            if(a[mid]== target)
-            {
-                return mid;
-            }
-            else if(a[mid]>target)
-            {
-                r=mid-1;
-            }
-            else
-            {
-                l=mid+1;
-            }
-        }
-        return -1;
-        
-    }
+        while(l<=r)
+        {
+            int mid = l+(r-l)/2;
+            if(a[mid]== target)
+            {
+                return mid;
+            }
+            else if(a[mid]>target)
+            {
+                r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return -1;
+        
+    }
 };
