e369e1c81feb4442f624adba020dd277ed9eb874
Cap-Panaka <Cap-Panaka@users.noreply.github.com>
2026-08-15 08:12:23 +0500

    [2095D - Where Am I? | Gym] Accepted | Time: 15ms | Memory: 0KB

---
+++ Codeforces/Others/2095D - Where Am I/2095D - Where Am I.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+void solve()
+{ 
+    cout<<36.104300 << " " << -115.172835;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+   // int t = 1;
+    //cin >> t;
+
+    //while(t--)
+    //{
+        solve();
+    //}
+
+    return 0;
+}
